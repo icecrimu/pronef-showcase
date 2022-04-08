8fa45806f9730c3f6f1aa94b85a89b517c016908
icecrimu <charles.banigon@gmail.com>
2022-04-08 19:36:34 +0800

    Render SectionWrapper in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { Download, Features, SectionWrapper } from "./components"
+import assets from "./assets"
 
 function App() {
-  return <></>
+  return (
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs."
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+    </>
+  )
 }
 
 export default App
