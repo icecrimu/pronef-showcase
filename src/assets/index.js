44529b518ad7b69008eaa4b1c35fc5b52b3de355
icecrimu <charles.banigon@gmail.com>
2022-04-08 22:46:39 +0800

    eslint quick fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,14 @@
-import homeHero from "./home_hero.png";
-import homeCards from "./home_cards.png";
-import feature from "./feature.png";
-import expo from "./expo.png";
-import expo02 from "./expo02.png";
-import javascript from "./javascript.png";
-import react from "./react.png";
-import scene from "./scene.png";
-import mockup from "./mockup.png";
+import homeHero from "./home_hero.png"
+import homeCards from "./home_cards.png"
+import feature from "./feature.png"
+import expo from "./expo.png"
+import expo02 from "./expo02.png"
+import javascript from "./javascript.png"
+import react from "./react.png"
+import scene from "./scene.png"
+import mockup from "./mockup.png"
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   homeHero,
   homeCards,
@@ -17,5 +18,5 @@ export default {
   javascript,
   react,
   scene,
-  mockup,
-};
+  mockup
+}
