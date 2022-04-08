ba17ca9024d5dd0057fac859e9dbf5807370dd48
icecrimu <charles.banigon@gmail.com>
2022-04-08 19:34:49 +0800

    Add title, description & button in SectionWrapper

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,7 +1,27 @@
 import React from "react"
 
-const SectionWrapper = () => {
-  return <div>SectionWrapper</div>
+import styles from "../styles/Global"
+import assets from "../assets"
+import Button from "./Button"
+
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+  return (
+    <div
+      className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
+    >
+      <div
+        className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}
+      >
+        <div className={`${styles.descDiv} `}>
+          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <p className={`${styles.descriptionText}`}>{description}</p>
+          {showBtn && (
+            <Button assetUrl={assets.expo} link="deployed nft marketplace rn" />
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SectionWrapper
