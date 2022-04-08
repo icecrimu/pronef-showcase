67a8daba9da3cac42f87ce913d5535292868b483
icecrimu <charles.banigon@gmail.com>
2022-04-08 20:52:07 +0800

    Add reverse prop in SectionWrapper

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,17 +4,43 @@ import styles from "../styles/Global"
 import assets from "../assets"
 import Button from "./Button"
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+const SectionWrapper = ({
+  title,
+  description,
+  showBtn,
+  mockupImg,
+  banner,
+  reverse
+}) => {
   return (
     <div
-      className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
+      className={`min-h-screen ${styles.section} 
+      ${reverse ? styles.bgWhite : styles.bgPrimary} 
+      ${banner}`}
     >
       <div
-        className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}
+        className={`flex items-center 
+        ${
+          reverse ? styles.boxReverseClass : styles.boxClass
+        } w-11/12 sm:w-full minmd:w-3/4`}
       >
-        <div className={`${styles.descDiv} `}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
-          <p className={`${styles.descriptionText}`}>{description}</p>
+        <div
+          className={`${styles.descDiv} 
+          ${reverse ? "fadeRightMini" : "fadeLeftMini"} 
+          ${reverse ? styles.textRight : styles.textLeft}`}
+        >
+          <h1
+            className={`${styles.h1Text} 
+            ${reverse ? styles.blackText : styles.whiteText}`}
+          >
+            {title}
+          </h1>
+          <p
+            className={`${styles.descriptionText} 
+            ${reverse ? styles.blackText : styles.whiteText} `}
+          >
+            {description}
+          </p>
           {showBtn && (
             <Button assetUrl={assets.expo} link="deployed nft marketplace rn" />
           )}
@@ -24,7 +50,8 @@ const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
           <img
             src={mockupImg}
             alt="mockup"
-            className={`${styles.sectionImg}`}
+            className={`${styles.sectionImg} 
+            ${reverse ? "fadeLeftMini" : "fadeRightMini"}`}
           />
         </div>
       </div>
